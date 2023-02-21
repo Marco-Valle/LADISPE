3fa4422c85ef078bf72412d53f8a81566f76898e
Marco Valle <marcovalle13@gmail.com>
2023-02-21 21:02:53 +0100

    Code refactoring of LADICourse.

---
+++ backend/ladicourses/coursesUtils.py
@@ -0,0 +1,137 @@
+from __future__ import annotations
+from backend.settings import MEDIA_ROOT, MEDIA_URL, FILEBROWSER_DIRECTORY, MAX_DEPTH_IN_MATERIAL_SEARCH
+from common.utils import safe_path
+from pathlib import Path
+from datetime import datetime
+from os import sep
+from re import sub
+from urllib.parse import quote
+from typing import List, Dict, Union, Optional, Callable
+
+from ladicourses.models import LADICourse
+from ladiusers.models import LADIUser
+
+
+# If a course is not marked as public, just the professor and his assistants can request it.
+restricted_course : Callable[[LADICourse, LADIUser], bool] = \
+    lambda course, user: not(user.is_superuser or course.public or user in {course.professor, course.first_assistant, course.second_assistant})
+
+
+class materialFile:
+    """File of a LADICourse's material."""
+    
+    name            :   str
+    url             :   str
+    path            :   Path
+    size            :   float
+    date            :   datetime
+    
+    
+    def __init__(self, path : Union[str, Path]) -> None:
+        """Initializer of the materialFile class."""
+        self.path = Path(path)
+        self.name = self.path.name
+        self.size = self.path.stat().st_size * 9.54e-7
+        self.date = datetime.fromtimestamp(self.path.stat().st_mtime).date()
+        url = sub(rf'^.*?{FILEBROWSER_DIRECTORY}', '', str(path).replace(sep, '/'))
+        self.url = str(quote("{}{}{}".format(MEDIA_URL, FILEBROWSER_DIRECTORY, url))).replace('%2F', '/', 1)
+        
+        
+    @property
+    def valid(self) -> bool:
+        "Check if it is a valid file."
+        return self.path.is_file()
+    
+    
+    def export(self) -> Optional[Dict[str, str]]:
+        """Export the file into a dictionary."""
+        return {
+            'name'  :   self.name,
+            'url'   :   self.url,
+            'size'  :   f'{self.size:.3f}',
+            'date'  :   self.date.strftime('%d-%m-%Y')
+        } if self.valid else None
+    
+
+class materialDirectory:
+    """Material directory class of a LADICourse."""
+    
+    title           :   str
+    path            :   Path
+    breadcrumbs     :   List[str]
+    files           :   List[materialFile]
+    dirs            :   List[materialDirectory]
+    
+    
+    def __init__(self, path : Union[str, Path]) -> None:
+        """Initializer of the materialDirectory class."""
+        self.path = Path(path)
+        self.title = self.path.name
+        breadcrumb_path = sub(rf'^.*?{FILEBROWSER_DIRECTORY}', '', str(path).replace(sep, '/'))
+        self.breadcrumbs = [folder for folder in breadcrumb_path.split(sep)][3:]
+        self.files = []
+        self.dirs = []
+        max_depth_reached = len(self.breadcrumbs) >= MAX_DEPTH_IN_MATERIAL_SEARCH
+        for child in self.path.iterdir():
+            if child.is_file():
+                self.files.append(materialFile(child))
+            elif child.is_dir() and not max_depth_reached:
+                self.dirs.append(materialDirectory(child))
+        
+        
+    @property
+    def valid(self) -> bool:
+        """Check if it is a valid directory."""
+        return self.path.is_dir()
+    
+
+
+class courseDirectory(materialDirectory):
+    """Root folder for a a LADICourse."""
+    
+    professor_id    :   int
+    course_title    :   str
+    iter_queue      :   List[materialDirectory]
+    
+    
+    def __init__(self, course_title, professor_id) -> None:
+        """Initializer of the courseDirectory class."""
+        self.professor_id = professor_id
+        self.course_title = course_title
+        self.iter_queue = []
+        path = safe_path(trusted_part=(MEDIA_ROOT, FILEBROWSER_DIRECTORY, 'Users'),
+                         untrusted_part=(str(professor_id), course_title))
+        materialDirectory.__init__(self, path=path)
+        self.iter_queue.append(self)
+    
+    
+    def __iter__(self) -> courseDirectory:
+        """Iterate between all the directories of the course."""
+        if not self.valid:
+            raise OSError(f"{self.path} is not a valid directory.")
+        return self
+        
+        
+    def __next__(self) -> materialDirectory:
+        """Get the next directory of the course."""
+        if not self.iter_queue:
+            raise StopIteration
+        next_dir = self.iter_queue.pop(0)
+        for dir in next_dir.dirs:
+            self.iter_queue.append(dir)
+        return next_dir
+        
+    
+    def export_material(self) -> List[Dict[str, Union[str, List[str], Dict[str, str]]]]:
+        """Export into a list of dictionaries all the directory of the course."""
+        result = []
+        for dir in self:
+            files = [file.export() for file in dir.files]
+            result.append({
+                'title'         :   dir.title,
+                'breadcrumbs'   :   dir.breadcrumbs,
+                'files'         :   files
+            })
+        result.sort(key=(lambda material: len(material['breadcrumbs'])), reverse=False)
+        return result
+    
--- backend/ladicourses/views.py
+++ backend/ladicourses/views.py
@@ -1,14 +1,8 @@
 from common.database import *
 from django.http import JsonResponse, HttpRequest
-from django.core.exceptions import ObjectDoesNotExist
-from backend.settings import MEDIA_ROOT, MEDIA_URL, FILEBROWSER_DIRECTORY, MAX_DEPTH_IN_MATERIAL_SEARCH
-from os import path, walk
-from urllib.parse import quote
-from time import strftime, localtime
 from filebrowser.base import FileObject
-from typing import List, Dict, Union
 
-from common.utils import safe_path
+from ladicourses.coursesUtils import courseDirectory, restricted_course
 from ladicourses.models import LADICourse, LADILecture
 
  
@@ -41,13 +35,8 @@ def get_courses_lectures(request: HttpRequest) -> JsonResponse:
     """Get all the LADILecture of a specific LADICourse (specified with GET parameters id)."""
     query = WebQuery(request=request, model=LADICourse, count=False)
     course = query.get_object_from_id(enforce_public=False)
-    if not course:
+    if not course or restricted_course(course=course, user=request.user):
         return JsonResponse([], safe=False)
-    if not course.public and not request.user.is_superuser:
-        # If a course is not marked as public, just the professor and his assistants can request it.
-        allowed_users = {course.professor, course.first_assistant, course.second_assistant}
-        if request.user not in allowed_users:
-            return JsonResponse([], safe=False)
     query_set = LADILecture.objects.filter(course_id=course.id)
     query_set = query_set.order_by('timestamp').reverse()
     data = list(query_set.values())
@@ -62,35 +51,15 @@ def get_courses_lectures(request: HttpRequest) -> JsonResponse:
 
 
 def get_courses_materials(request: HttpRequest) -> JsonResponse:
+    """Get the material of a specific LADICourse (specified with GET parameters id)."""
     query = WebQuery(request=request, model=LADICourse, count=False)
-    course = query.get_object_from_id()
-    materials_path = courses_get_folder(course.id)
-    if not materials_path:
+    course = query.get_object_from_id(enforce_public=False)
+    if not course or restricted_course(course=course, user=request.user):
         return MATERIAL_EMPTY_RESPONSE
-    root_files = next(walk(materials_path), (None, None, []))[2]    # Add root directory
-    result = [{'title': '', 'breadcrumbs': [],
-               'files': [courses_pack_file(file, materials_path) for file in root_files]}]
-    courses_recursive_browse(result, materials_path, MAX_DEPTH_IN_MATERIAL_SEARCH, [])
-    courses_sort_material(result)
-    return JsonResponse(result, safe=False)
-
-
-def courses_recursive_browse(dirs_list: List[Dict[str, Union[str, List[str], List[Dict[str, str]]]]],
-                             parent: str,
-                             depth: int,
-                             breadcrumbs: List[str]
-                             ) -> List[Dict[str, str]]:
-    parent_dir = next(walk(parent), (None, None, []))
-    if depth:
-        for topic_title in parent_dir[1]:
-            # For each subdirectory
-            crumb = path.join(parent, topic_title)
-            crumb = crumb.split(path.sep)
-            breadcrumbs.append(str(crumb[-1]))
-            dirs_list.append({'title': topic_title, 'breadcrumbs': breadcrumbs.copy(),
-                              'files': courses_recursive_browse(dirs_list, path.join(parent, topic_title), depth - 1, breadcrumbs)})
-            breadcrumbs.pop()
-    return [courses_pack_file(file, parent) for file in parent_dir[2]]
+    course_foder = courseDirectory(course_title=course.title, professor_id=course.professor.id)
+    if not course_foder.valid:
+        return MATERIAL_EMPTY_RESPONSE
+    return JsonResponse(course_foder.export_material(), safe=False)
 
 
 def courses_retrieve_results(query: WebQuery) -> QuerySet:
@@ -104,47 +73,6 @@ def courses_retrieve_results(query: WebQuery) -> QuerySet:
     if query.public:
         return query_set.filter(public__exact=True)
     return query_set
-
-
-def courses_get_folder(course_id: int) -> Optional[str]:
-    try:
-        course = LADICourse.objects.get(id=course_id)
-    except ObjectDoesNotExist:
-        return None
-    course_dir = safe_path( trusted_part=(MEDIA_ROOT, FILEBROWSER_DIRECTORY, 'Users'),
-                            untrusted_part=(str(course.professor_id), course.title))
-    if not path.isdir(course_dir):
-        return None
-    return course_dir
-
-
-def courses_get_file_url(parent_path: str, file_name: str) -> str:
-    parent = parent_path[parent_path.find(FILEBROWSER_DIRECTORY) + len(FILEBROWSER_DIRECTORY):]
-    url = str(quote("{}{}{}/{}".format(MEDIA_URL, FILEBROWSER_DIRECTORY, parent, file_name).replace('\\', '/')))
-    return url.replace('%2F', '/', 1)
-
-
-def courses_get_file_date(parent: str, filename: str) -> str:
-    epoch = path.getmtime(path.join(parent, filename))
-    return strftime('%d-%m-%Y', localtime(epoch))
-
-
-def courses_get_file_size(parent: str, filename: str) -> str:
-    size = path.getsize(path.join(parent, filename))
-    return "{:.3f}".format(size * 9.54e-7)
-
-
-def courses_pack_file(file: str, parent: str) -> List[Dict[str, str]]:
-    return {
-        'name': file,
-        'url': courses_get_file_url(parent, file),
-        'date': courses_get_file_date(parent, file),
-        'size': courses_get_file_size(parent, file)}
-
-
-def courses_sort_material(materials: List[Dict[str, Union[str, List[str], List[Dict[str, str]]]]]
-                          ) -> List[Dict[str, Union[str, List[str], List[Dict[str, str]]]]]:
-    materials.sort(key=(lambda material: len(material['breadcrumbs'])), reverse=False)
 
 
 # LADILecture
