a832736003482a9e561f1bd1e4611c4dc3c1c274
Marco Valle <marcovalle13@gmail.com>
2023-02-23 13:51:27 +0100

    Private email in LADICourse implemented and code refactoring for LADIUser.

--- backend/ladiusers/views.py
+++ backend/ladiusers/views.py
@@ -2,6 +2,7 @@ from django.core.exceptions import SuspiciousOperation, ObjectDoesNotExist
 from django.http import JsonResponse, HttpRequest
 from typing import Dict
 
+from common.database import *
 from ladiusers.models import LADIUser
 from ladicontent.models import LADIStaff
 from ladicourses.models import LADICourse
@@ -10,58 +11,35 @@ from ladicourses.models import LADICourse
 def get_user(request: HttpRequest) -> JsonResponse:
     """Get a LADIUSer serialized as json from a HTTP request."""
     
-    # Only user searches by ID are allowed
-    parameters = get_request(request)
-    if 'id' not in parameters:
+    query = WebQuery(request=request, model=LADIUser)
+    if query.type != QueryType.BY_ID:
+        # Only user searches by ID are allowed
         return JsonResponse([], safe=False)
-    try:
-        my_id = int(parameters['id'][0])
-        user = search_by_id(int(parameters['id'][0]))
-    except ValueError:
+    
+    user = query.get_object_from_id(enforce_public=False)
+    if not user:
+        return JsonResponse([], safe=False)
+    
+    if not(request.user.is_superuser or request.user == user or 'ladiusers.view_LADIUser' in request.user.get_group_permissions()):
+        try:
+            # Is the requested user a staff member?
+            LADIStaff.objects.get(user_id=query.obj_id)
+            restricted_user = False
+        except ObjectDoesNotExist:
+            # Is the requested user a staff member of some courses?
+            query_set = LADICourse.objects.filter(professor_id=query.obj_id, private_email=False)
+            query_set = LADICourse.objects.filter(first_assistant_id=query.obj_id, private_email=False).union(query_set)
+            query_set = LADICourse.objects.filter(second_assistant_id=query.obj_id, private_email=False).union(query_set)
+            restricted_user = False if query_set and query_set.count() else True
+
+    if restricted_user:
         return JsonResponse([], safe=False)
 
-    # Is the user who has done the request allowed to retrieve info about this user?
-    user_allowed = request.user.is_superuser or request.user == user or 'ladiusers.view_LADIUser' in request.user.get_group_permissions()
-    try:
-        # Is the requested user a staff member?
-        LADIStaff.objects.get(user_id=my_id)
-        user_visible = True
-    except ObjectDoesNotExist:
-        # Is the requested user a staff member of some courses?
-        query_set = LADICourse.objects.filter(professor_id=my_id)
-        query_set = LADICourse.objects.filter(first_assistant_id=my_id).union(query_set)
-        query_set = LADICourse.objects.filter(second_assistant_id=my_id).union(query_set)
-        if query_set:
-            user_visible = True
-        else:
-            # If the user has no reason to be public, protects his and our privacy
-            user_visible = False
-    if user and (user_allowed or user_visible):
-        # The if above is used to protect us from the user enumeration
-        # Don't send the entire user, sensitive infos may be leaked (password hash, privileges...)
-        result = {
-            'id': my_id,
-            'name': user.name,
-            'surname': user.surname,
-            'email': user.email,
-        }
-        return JsonResponse(result, safe=True)
-    return JsonResponse([], safe=False)
-
-
-def get_request(request: HttpRequest) -> Dict[str, str]:
-    """Check for GET request and parse the parameters into a dict."""
-    if request.method != 'GET':
-        raise SuspiciousOperation("GET requests only")
-    # Get the parameters from the request
-    return request.GET.dict()
-
-
-def search_by_id(my_id: int) -> LADIUser:
-    """Get a LADIUser from its id."""
-    try:
-        return LADIUser.objects.get(id=my_id)
-    except ObjectDoesNotExist:
-        return None
-    except ValueError:
-        raise SuspiciousOperation("Wrong type")
+    # Don't send the entire user, sensitive infos may be leaked (password hash, privileges...)
+    result = {
+        'id': query.obj_id,
+        'name': user.name,
+        'surname': user.surname,
+        'email': user.email,
+    }
+    return JsonResponse(result, safe=True)
